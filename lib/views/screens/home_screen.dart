f3ab8273cd4e619f6f8e71a1f054df02fb8f7f8d
Golam Sawer <golamsarwer97@gmail.com>
2023-04-23 22:22:44 +0600

    added bottom nav bar & pages

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -1,15 +1,59 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+import '.././widgets/custom_icon.dart';
+import '../../constants.dart';
+
+class HomeScreen extends StatefulWidget {
   static const routeName = '/home-screen';
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int pageIdx = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: backgroundColor,
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.white,
+        onTap: (idx) {
+          setState(() {
+            pageIdx = idx;
+          });
+        },
+        currentIndex: pageIdx,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_filled),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: CustomIcon(),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            label: 'Message',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
       ),
+      body: pages[pageIdx],
     );
   }
 }

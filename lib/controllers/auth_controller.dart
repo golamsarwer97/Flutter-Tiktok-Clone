ff54e356b2ae5a5d01e7ad779627d4e6c59e4539
Golam Sawer <golamsarwer97@gmail.com>
2023-04-23 21:37:56 +0600

    added user state, signUp & signIn function

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,12 +1,52 @@
+// ignore_for_file: unnecessary_null_comparison, avoid_print
+
 import 'dart:io';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
+import '../views/screens/auth/login_screen.dart';
+import '../views/screens/home_screen.dart';
 import '../constants.dart';
+import '../models/user.dart';
 
 class AuthController extends GetxController {
+  static AuthController instance = Get.find();
+
+  late Rx<User?> _user;
+  User get user => _user.value!;
+
+  @override
+  onReady() {
+    super.onReady();
+    _user = Rx<User?>(auth.currentUser);
+    _user.bindStream(auth.authStateChanges());
+    ever(_user, _setInitialScreen);
+  }
+
+  _setInitialScreen(User? user) {
+    if (user == null) {
+      Get.offAll(() => const LoginScreen());
+    } else {
+      Get.offAll(() => const HomeScreen());
+    }
+  }
+
+  Rx<File?>? _pickedImage;
+  File? get profileImage => _pickedImage!.value;
+
+  void pickImage() async {
+    final pickedImage =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedImage != null) {
+      Get.snackbar('Profile Picture',
+          'You have successfully selected your profile picture!');
+    }
+    _pickedImage = Rx<File?>(File(pickedImage!.path));
+  }
+
   // upload data to firebase Storage
   Future<String> _uploadToStorage(File image) async {
     Reference ref =
@@ -37,9 +77,37 @@ class AuthController extends GetxController {
         );
 
         String downloadUrl = await _uploadToStorage(image);
+        UserModel user = UserModel(
+          uid: userCredential.user!.uid,
+          name: username,
+          profileImage: downloadUrl,
+          email: email,
+        );
+        await firestore
+            .collection('users')
+            .doc(userCredential.user!.uid)
+            .set(user.toJson());
+      } else {
+        Get.snackbar('Error Creating Account', 'Please enter all fields');
       }
     } catch (e) {
       Get.snackbar('Error Creating Account', e.toString());
+    }
+  }
+
+  void login(String email, String password) async {
+    try {
+      if (email.isNotEmpty && password.isNotEmpty) {
+        await auth.signInWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+        print('logging successfully');
+      } else {
+        Get.snackbar('Error on logging', 'Please enter all fields');
+      }
+    } catch (e) {
+      Get.snackbar('Error on logging', e.toString());
     }
   }
 }
